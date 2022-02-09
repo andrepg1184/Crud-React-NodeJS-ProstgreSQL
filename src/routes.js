11048545d2ee80a476c5dd20c74165e738f53a21
André leite <andreleite1184@hotmail.com>
2022-02-08 23:04:17 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const todosRoute = express.Router()
+
+//C
+todosRoutes.post("/todos", (request, response) =>{
+    const {name = response.body;
+    }
+})
+//R
+//U
+//D
